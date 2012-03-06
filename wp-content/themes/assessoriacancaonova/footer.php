93403fb79a058d8d423d1f9f433ac202bc388073
daviddisans@gmail.com <daviddisans@gmail.com>
2012-03-06 16:13:55 -0300

    inicio css footer]

--- wp-content/themes/assessoriacancaonova/footer.php
+++ wp-content/themes/assessoriacancaonova/footer.php
@@ -14,16 +14,18 @@
 		</div><!-- #main -->
 
 		<div id="container-footer">
-			<footer role="contentinfo">
-			<?php
-				/* A sidebar in the footer? Yep. You can can customize
-				 * your footer with four columns of widgets.
-				 */
-				get_sidebar( 'footer' );
-			?>
-				<a href="<?php echo home_url( '/' ) ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>
-				<a href="http://wordpress.org/" title="Semantic Personal Publishing Platform" rel="generator">Proudly powered by WordPress </a>
-			</footer><!-- footer -->
+			<div id="content-footer">
+				<footer role="contentinfo">
+				<?php
+					/* A sidebar in the footer? Yep. You can can customize
+					 * your footer with four columns of widgets.
+					 */
+					get_sidebar( 'footer' );
+				?>
+					<a href="<?php echo home_url( '/' ) ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>
+					<a href="http://wordpress.org/" title="Semantic Personal Publishing Platform" rel="generator">Proudly powered by WordPress </a>
+				</footer><!-- footer -->
+			</div> <!-- #content-footer -->
 		</div> <!-- #container-footer -->
 
 		<?php
