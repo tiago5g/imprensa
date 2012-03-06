d1094797b88fc5cf019bad5fb92fed1b14630439
daviddisans@gmail.com <daviddisans@gmail.com>
2012-03-06 16:25:46 -0300

    gitignore

--- wp-config.php
+++ wp-config.php
@@ -22,7 +22,7 @@ define('DB_NAME', 'imprensa');
 define('DB_USER', 'root');
 
 /** MySQL database password */
-define('DB_PASSWORD', '123456');
+define('DB_PASSWORD', '');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
