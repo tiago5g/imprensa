aaf884da98711e0179dc2d4091d1cf174c3c4141
tiagor <tiagor@jabber.joaopauloii>
2012-03-06 14:44:09 -0300

    exibir 03 últimos posts da categoria artigos e últimos da categoria pautas na index. criação de arquivos para loops específicos. modificação de conteúdo do home, para seus respectivos loops. Acrescentando plugin para campos customizados, também usado na FCN. Acrescentando código para exibição de notas oficiais na home. Pendente de css.

--- wp-content/themes/assessoriacancaonova/home.php
+++ wp-content/themes/assessoriacancaonova/home.php
@@ -14,14 +14,10 @@
  */
 
 get_header(); ?>
-<<<<<<< HEAD
 		<div id="container-content" role="main">
                         <section id="nota-oficial">
                                 <?php get_template_part( 'loop', 'notas' );?>
                         </section>
-=======
-	
->>>>>>> 6fbb202c397fb632bc6bafb21670e4470b87f4ec
 			<section id="menu-rapido">
 				<header>
 					<h3>Acesso Rápido</h3>
@@ -31,59 +27,8 @@ get_header(); ?>
 			<section id="ultimas-pautas">
                                 <?php get_template_part( 'loop', 'pautas' );?>
 			</section><!-- #ultimas-pautas -->
-
-<<<<<<< HEAD
 			<section id="ultimos-artigos">				
 				<?php get_template_part( 'loop', 'artigos' );?>
-=======
-			<section id="ultimos-artigos">
-				<header>
-					<h3>Artigos</h3>
-				</header>
-				
-				<article>
-					<div>
-						<time>01/01/2011</time>
-						<span>Categoria</span>
-					</div>
-					<a href="#" title="Titulo do Artigo">
-						<h4>Titulo do Artigo</h4>
-						<div>
-							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam porta ante vitae augue fermentum non volutpat nisl porta.</p>
-						</div>
-					</a>
-				</article>
-
-				<article>
-					<div>
-						<time>01/01/2011</time>
-						<span>Categoria</span>
-					</div>
-					<a href="#" title="Titulo do Artigo">
-						<h4>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h4>
-						<div>
-							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam porta ante vitae augue fermentum non volutpat nisl porta.</p>
-						</div>
-					</a>
-				</article>
-
-				<article>
-					<div>
-						<time>01/01/2011</time>
-						<span>Categoria</span>
-					</div>
-					<a href="#" title="Titulo do Artigo">
-						<h4>Titulo do Artigo</h4>
-						<div>
-							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam porta ante vitae augue fermentum non volutpat nisl porta.</p>
-						</div>
-					</a>
-				</article>
-				
-				<footer>
-					<span><a href="#" title="">veja mais</a></span>
-				</footer>
->>>>>>> 6fbb202c397fb632bc6bafb21670e4470b87f4ec
 			</section><!-- #ultimos-artigos -->
 
 			<section id="destaque-fotos">
