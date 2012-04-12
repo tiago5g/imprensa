8e07080117392dcb9c2fb04cf606b3f763a2026e
tiagor <lordtiago@msn.com>
2012-04-12 09:47:15 -0300

    plugin flickr

---
+++ wp-content/plugins/new-contents/Views/Pages/add_numbers.php
@@ -0,0 +1,3 @@
+<h6>Digite um dado numérico relacionado à página</h6>
+<input name="numeros" id="numeros" value="<?php echo get_post_meta( $page->ID, 'numeros', true );?>" />
+<h6>Ex.: Página TV = 1000 transmissoras no Brasil</h6>
