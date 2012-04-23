2c65434281e15d875bbef97a86ca248bfedbf18d
Thiago Zucareli <thiagozp@gmail.com>
2012-04-23 16:56:53 -0300

    Atualização WordPress; Adicionado: Lista de Artigos, Pautas e Nas Midias, Perfil do evento

---
+++ wp-content/themes/assessoriacancaonova/archive-nas-midias.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * The template for displaying Archive pages.
+ *
+ * Used to display archive-type pages if nothing more specific matches a query.
+ * For example, puts together date-based pages if no date.php file exists.
+ *
+ * Learn more: http://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package WordPress
+ * @subpackage Boilerplate
+ * @since Boilerplate 1.0
+ */
+
+get_header(); ?>
+
+	<div id="container-content" class="main">
+		<div class="nasmidias">
+
+			<h1>Nas Midias</h1>
+			
+			<ul >	
+				<li>
+					<time>12/12/2012</time>
+					<a href="#">
+						<h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h2>
+					</a>
+					<span>Midia: </span>
+					<span>Formato: </span>
+				</li><!-- pauta -->
+
+				<li>
+					<time>12/12/2012</time>
+					<a href="#">
+						<h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h2>
+					</a>
+					<span>Midia: </span>
+					<span>Formato: </span>
+				</li><!-- pauta -->
+
+				<li>
+					<time>12/12/2012</time>
+					<a href="#">
+						<h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h2>
+					</a>
+					<span>Midia: </span>
+					<span>Formato: </span>
+				</li><!-- pauta -->
+
+				<li>
+					<time>12/12/2012</time>
+					<a href="#">
+						<h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h2>
+					</a>
+					<span>Midia: </span>
+					<span>Formato: </span>
+				</li><!-- pauta -->
+			</ul>
+
+				<a href="#" class="ver-mais">Ver mais</a>
+
+		</div><!-- lista-pautas -->
+	</div> <!-- main -->
+
+<?php get_footer(); ?>
---
+++ wp-content/themes/assessoriacancaonova/archive-pauta.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * The template for displaying Archive pages.
+ *
+ * Used to display archive-type pages if nothing more specific matches a query.
+ * For example, puts together date-based pages if no date.php file exists.
+ *
+ * Learn more: http://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package WordPress
+ * @subpackage Boilerplate
+ * @since Boilerplate 1.0
+ */
+
+get_header(); ?>
+
+	<div id="container-content" class="main">
+		<div class="lista">
+
+			<h1>Pautas</h1>
+			
+			<ul >	
+				<li>
+					<time>12/12/2012</time>
+					<a href="#">
+						<h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h2>
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse nunc lacus, ultrices non bibendum et, rutrum ac justo. Sed vitae turpis mauris, adipiscing lobortis lectus. Ut nec auctor turpis. Donec eleifend viverra massa sed pretium. Proin elit sem, gravida eu …</p>
+					</a>
+				</li><!-- pauta -->
+
+				<li>
+					<time>12/12/2012</time>
+					<a href="#">
+						<h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h2>
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse nunc lacus, ultrices non bibendum et, rutrum ac justo. Sed vitae turpis mauris, adipiscing lobortis lectus. Ut nec auctor turpis. Donec eleifend viverra massa sed pretium. Proin elit sem, gravida eu …</p>
+					</a>
+				</li><!-- pauta -->
+
+				<li>
+					<time>12/12/2012</time>
+					<a href="#">
+						<h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h2>
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse nunc lacus, ultrices non bibendum et, rutrum ac justo. Sed vitae turpis mauris, adipiscing lobortis lectus. Ut nec auctor turpis. Donec eleifend viverra massa sed pretium. Proin elit sem, gravida eu …</p>
+					</a>
+				</li><!-- pauta -->
+
+				<li>
+					<time>12/12/2012</time>
+					<a href="#">
+						<h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h2>
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse nunc lacus, ultrices non bibendum et, rutrum ac justo. Sed vitae turpis mauris, adipiscing lobortis lectus. Ut nec auctor turpis. Donec eleifend viverra massa sed pretium. Proin elit sem, gravida eu …</p>
+					</a>
+				</li><!-- pauta -->
+			</ul>
+
+				<a href="#" class="ver-mais">Ver mais</a>
+
+		</div><!-- lista-pautas -->
+	</div> <!-- main -->
+
+<?php get_footer(); ?>
